e2d2d484e21b13f9f75a21450e1798b5c79a76f4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2021-10-05 11:13:12 +0700

    Create app2.py
    Code has been refactored.

---
+++ app2.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown('''# **Binance Price App**
+A simple cryptocurrency price app pulling price data from *Binance API*.
+''')
+
+st.header('**Selected Price**')
+
+# Load market data from Binance API
+df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
+# Custom function for rounding values
+def round_value(input_value):
+    if input_value.values > 1:
+        a = float(round(input_value, 2))
+    else:
+        a = float(round(input_value, 8))
+    return a
+
+crpytoList = {
+    'Price 1': 'BTCBUSD',
+    'Price 2': 'ETHBUSD',
+    'Price 3': 'BNBBUSD',
+    'Price 4': 'XRPBUSD',
+    'Price 5': 'ADABUSD',
+    'Price 6': 'DOGEBUSD',
+    'Price 7': 'SHIBBUSD',
+    'Price 8': 'DOTBUSD',
+    'Price 9': 'MATICBUSD'
+}
+
+col1, col2, col3 = st.columns(3)
+
+for i in range(len(crpytoList.keys())):
+    selected_crypto_label = list(crpytoList.keys())[i]
+    #selected_crypto_value = crpytoList[selected_crypto_label]
+    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+    selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(i))
+    col_df = df[df.symbol == selected_crypto]
+    col_price = round_value(col_df.weightedAvgPrice)
+    col_percent = f'{float(col_df.priceChangePercent)}%'
+    if i < 3:
+        with col1:
+            st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+        with col2:
+            st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+        with col3:
+            st.metric(selected_crypto, col_price, col_percent)
+
+st.header('**All Price**')
+st.dataframe(df)
+
+st.info('Credit: Created by Chanin Nantasenamat (aka [Data Professor](https://youtube.com/dataprofessor/))')
+
+st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
